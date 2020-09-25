312af62dafebf196fe9444da54ffdaf8ca5334ce
Elke Boonen <u0116591@thomasmore.be>
2020-09-25 10:29:56 +0200

    Hello World!

---
+++ HelloWorld/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace HelloWorld
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!"); //text between " " 
+            //We import the System namespace by using-statement
+            System.Console.Write('h'); //single characters between ' ' 
+            //write text without newlines!
+            Console.Write(' ');
+            Console.Write('e');
+            Console.Write(' ');
+            Console.Write('l');
+            Console.Write(' ');
+            Console.Write('l');
+            Console.Write(' ');
+            System.Console.WriteLine('o');
+            //numbers without quotes
+            Console.WriteLine(0);
+            //decimal point, not a comma
+            Console.WriteLine(0.5);
+            //true or false without quotes
+            Console.WriteLine(true);
+            Console.WriteLine(false);
+
+        }
+    }
+}
