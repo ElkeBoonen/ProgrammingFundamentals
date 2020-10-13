83492e526ac62be40a129d92f741fa129c8ef84c
Elke Boonen <u0116591@thomasmore.be>
2020-10-13 11:35:34 +0200

    Only half of programming is coding. The other 90% is debugging.

---
+++ ConditionalStatements/Program.cs
@@ -0,0 +1,153 @@
+﻿using System;
+
+namespace ConditionalStatements
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("x :");
+            string answer = Console.ReadLine();
+            int x;
+            bool succes = Int32.TryParse(answer, out x);
+
+            if (!succes)
+            {
+                Console.WriteLine("Oopsie, try again");
+                return;
+            }
+            
+            int y = 18;
+
+            //variation 1
+            if (x > y)
+            {
+                Console.WriteLine("x is greater than y");
+            }
+            else if (x == y)
+            {
+                Console.WriteLine("x is equal to y");
+            }
+            else
+            {
+                Console.WriteLine("x is less than y");
+            }
+
+            //variation 2
+            if (x > y)
+            {
+                Console.WriteLine("x is greater than y");
+            }
+            else if (x < y)
+            {
+                Console.WriteLine("x is less than y");  
+            }
+            else
+            {
+                Console.WriteLine("x is equal to y");
+            }
+
+            //variation 3 - nest if structures
+            if (x > y)
+            {
+                Console.WriteLine("x is greater than y");
+            }
+            else
+            {
+                if (x < y)
+                {
+                    Console.WriteLine("x is less than y");
+                }
+                else
+                {
+                    Console.WriteLine("x is equal to y");
+                }
+            }
+
+            //variation 4
+            if ((x < y) || (x > y))
+            {
+                Console.WriteLine("x is not equal to y");
+            }
+            else
+            {
+                Console.WriteLine("x is equal to y");
+            }
+
+            //variation 5
+            if (x!=y)
+            {
+                Console.WriteLine("x is not equal to y");
+            }
+            else
+            {
+                Console.WriteLine("x is equal to y");
+            }
+
+            //variation 6
+            bool result = x != y;
+            if (result)
+            {
+                Console.WriteLine("x is not equal to y");
+            }
+            else
+            {
+                Console.WriteLine("x is equal to y");
+            }
+
+            DateTime today = DateTime.Now;
+            int weekday = Convert.ToInt32(today.DayOfWeek);
+            Console.WriteLine(weekday);
+
+            if (weekday == 1)
+            {
+                Console.WriteLine("It's Monday");
+            }
+            else if (weekday == 2)
+            {
+                Console.WriteLine("It's Tuesday");
+            }
+            else if (weekday == 3)
+            {
+                Console.WriteLine("It's Wednesday");
+            }
+            else if (weekday == 4)
+            {
+                Console.WriteLine("It's Thursday");
+            }
+            else if (weekday == 5)
+            {
+                Console.WriteLine("It's Friday");
+            }
+            else if ((weekday == 6) || (weekday == 7))
+            {
+                Console.WriteLine("It's Weekend");
+            }
+            else
+            {
+                Console.WriteLine("It's a crazy day");
+            }
+
+            switch (weekday)
+            {
+                case 1: Console.WriteLine("It's Monday");
+                    break;
+                case 2: Console.WriteLine("It's Tuesday");
+                    break;
+                case 3: Console.WriteLine("It's Wednesday");
+                    break;
+                case 4: Console.WriteLine("It's Thursday");
+                    break;
+                case 5: Console.WriteLine("It's Friday");
+                    break;
+                case 6:
+                case 7: Console.WriteLine("It's Weekend");
+                    break;
+                default: Console.WriteLine("It's a crazy day");
+                    break;
+            }
+
+
+        }
+    }
+}
---
+++ Loops/Program.cs
@@ -0,0 +1,103 @@
+﻿using System;
+using System.Runtime.CompilerServices;
+
+namespace Loops
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("x? ");
+            int x = Convert.ToInt32(Console.ReadLine());
+            int result;
+
+            //increment
+            Console.WriteLine("FOR - 0 <= 10");
+            for (int i = 0; i <= 10; i++)
+            {
+                result = i * x;
+                Console.WriteLine("{0} * {1} = {2}", i, x, result);
+            }
+
+            //decrement
+            Console.WriteLine("\n\nFOR - 10 >= 0");
+            for (int i = 10; i >= 0; i--)
+            {
+                result = i * x;
+                Console.WriteLine("{0} * {1} = {2}", i, x, result);
+            }
+
+            //step size 2
+            Console.WriteLine("\n\nFOR - 0 <= 10 - step size 2");
+            for (int i = 0; i <= 10; i=i+2) //i+=2
+            {
+                result = i * x;
+                Console.WriteLine("{0} * {1} = {2}", i, x, result);
+            }
+
+            //table of stars
+            Console.WriteLine("\n\nNESTED FOR LOOPS");
+            for (int i = 0; i < 5; i++)
+            {
+                for (int j = 0; j < 7; j++)
+                {
+                    Console.Write("* ");
+                }
+                Console.WriteLine();
+            }
+
+            //ask the user to continue
+            Console.WriteLine("\n\nWHILE - ask user for input");
+            Console.Write("Next? yes to continue!: ");
+            string answer = Console.ReadLine();
+            while (answer == "yes")
+            {
+                Console.Write("Next? yes to continue!: ");
+                answer = Console.ReadLine();
+            }
+
+
+            //3^0 3^1 3^2 3^3...
+            //1 3 9 81...
+            //10^0 10^1 10^2 10^3...
+            //1 10 100 1000...
+            Console.Write("\n\nWHILE - start with 1 (print before): ");
+            result = 1;
+            while (result < 10000)
+            {
+                if ((x == 0) || (Math.Abs(x) == 1)) break;
+                Console.Write(result + " ");
+                result = result * x;
+            }
+
+            Console.Write("\n\nWHILE - start with 1 (print after): ");
+            result = 1;
+            while (result < 10000)
+            {
+                if ((x == 0) || (Math.Abs(x) == 1)) break;
+                result = result * x;
+                Console.Write(result + " ");
+            }
+
+            Console.Write("\n\nWHILE - start with x (print before): ");
+            result = x;
+            while (result < 10000)
+            {
+                if ((x == 0) || (Math.Abs(x) == 1)) break;
+                Console.Write(result + " ");
+                result = result * x;
+            }
+
+
+            Console.Write("\n\nDO WHILE - start with x (print before): ");
+            result = x;
+            do
+            {
+                if ((x == 0) || (Math.Abs(x) == 1)) break;
+                Console.Write(result + " ");
+                result = result * x;
+            } while (result < 10000) ;
+
+        }
+    }
+}
